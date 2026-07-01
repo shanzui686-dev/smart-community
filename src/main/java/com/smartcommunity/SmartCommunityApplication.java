50879587eafa68196aeaaa88041c83a6fb368dbb
goodness-and-sin <2377420436@qq.com>
2026-07-01 18:13:54 +0800

    chore: 完善 .gitignore 规则并优化启动类

--- src/main/java/com/smartcommunity/SmartCommunityApplication.java
+++ src/main/java/com/smartcommunity/SmartCommunityApplication.java
@@ -1,13 +1,24 @@
 package com.smartcommunity;
 
+import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+/**
+ * 智慧社区管理系统启动类
+ *
+ * @author Smart Community Team
+ * @since 2024-01-01
+ */
 @SpringBootApplication
+@MapperScan("com.smartcommunity.mapper")
 public class SmartCommunityApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(SmartCommunityApplication.class, args);
+        System.out.println("========================================");
+        System.out.println("  智慧社区管理系统启动成功！");
+        System.out.println("  访问地址: http://localhost:8087/api");
+        System.out.println("========================================");
     }
-
 }
