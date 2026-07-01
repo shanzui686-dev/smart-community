92225002027af4039ab10e43c2c3d2072f2db8e3
goodness-and-sin <2377420436@qq.com>
2026-07-01 18:06:23 +0800

    Initial commit: Smart Community project (Spring Boot)

---
+++ src/test/java/com/smartcommunity/SmartCommunityApplicationTests.java
@@ -0,0 +1,13 @@
+package com.smartcommunity;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SmartCommunityApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
